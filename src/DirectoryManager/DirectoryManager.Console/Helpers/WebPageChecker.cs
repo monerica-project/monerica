7fd5945c831a43f959f134deea7c4e46a58ebbc0
Ryan Williams <ryanmichaelwilliams@gmail.com>
2023-09-13 10:00:40 -0600

    use default header

--- src/DirectoryManager/DirectoryManager.Console/Helpers/WebPageChecker.cs
+++ src/DirectoryManager/DirectoryManager.Console/Helpers/WebPageChecker.cs
@@ -3,11 +3,14 @@
 
     public class WebPageChecker
     {
+        const string DefaultHeader = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36";
+
         public static async Task<bool> IsWebPageOnlineAsync(Uri uri)
         {
             using HttpClient client = new();
             try
             {
+                client.DefaultRequestHeaders.UserAgent.ParseAdd(DefaultHeader);
                 HttpResponseMessage response = await client.GetAsync(uri);
 
                 var responseCode = response.StatusCode;
