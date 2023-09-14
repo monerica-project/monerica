6a4784c887157668f7d4c63c6dc8dbe7e1eb7ed5
Ryan Williams <ryanmichaelwilliams@gmail.com>
2023-09-14 15:27:09 -0600

    show differences on edit submission  (#114)
    * 2022-06-28 updates
    
    * abc order/ readme
    
    * readme update
    
    * readme
    
    * readme
    
    * 2022-06-22 updates
    
    * use HTML encoding
    
    * Monero Bounties
    
    * readme
    
    * readme
    
    * Monero Gear
    
    * Monero Gateway processor
    
    * 2022-08-24 updates
    
    * fix link up
    
    * cake pay
    
    * confirm cake pay
    
    * Hackers For Liberty
    
    * confirm all ark
    
    * move to communities
    
    * sethforprivacy.com
    
    * doante link and date update
    
    * Kwenta
    
    * approve mile high
    
    * breaking monero
    
    * update 2022-09-22
    
    * 2022-09-22 updates
    
    * permanentresidencyparaguay.com
    
    * fix spelling
    
    * show success when it ends
    
    * ignore
    
    * edit existing entries
    
    * app configs output fix
    
    * api to return entries
    
    * ignore settings
    
    * check links/ fix ordering
    
    * use default header
    
    * ignore null entry
    
    * force https and redirct www
    
    * check links with possible failure
    
    * ignore removed entries
    
    * guidelines message
    
    * show category details in list
    
    * allow for entry status change suggestion
    
    * api output cache
    
    * audit table
    
    * remove large file
    
    * newest additions/ category create
    
    * text/ display updates
    
    * admin links
    
    * group new entries by date
    
    * newest additions on home page
    
    * conditional display formatting
    
    * verify there are changes before creating submission
    
    * prevent unnecessary revision submissions
    
    * preformatted text for copying
    
    * include audit table
    
    * formatting
    
    * display differences on edited entries

--- src/DirectoryManager/DirectoryManager.Web/Controllers/SubmissionController.cs
+++ src/DirectoryManager/DirectoryManager.Web/Controllers/SubmissionController.cs
@@ -225,6 +225,12 @@ namespace DirectoryManager.Web.Controllers
         public async Task<IActionResult> Edit(int id)
         {
             var submission = await _submissionRepository.GetByIdAsync(id);
+
+            if (submission.DirectoryEntryId != null)
+            {
+                var existing = await _directoryEntryRepository.GetByIdAsync(submission.DirectoryEntryId.Value);
+                ViewBag.Differences = CompareEntries(existing, submission);
+            }
             
             var subCategories = (await _subCategoryRepository.GetAllAsync())
               .OrderBy(sc => sc.Category.Name)
@@ -336,6 +342,33 @@ namespace DirectoryManager.Web.Controllers
 
             return View(model);
         }
+
+        public static string CompareEntries(DirectoryEntry entry, Submission submission)
+        {
+            if (entry == null || submission == null)
+                return "Either the DirectoryEntry or the Submission is null.";
+
+            // Helper function to compare trimmed strings, considering null values.
+            bool NotEqualTrimmed(string? a, string? b)
+            {
+                return (a?.Trim() ?? string.Empty) != (b?.Trim() ?? string.Empty);
+            }
+            var differences = new List<string>();
+
+            if (NotEqualTrimmed(entry.Name, submission.Name)) differences.Add($"Different Name: {entry.Name ?? "null"} vs {submission.Name ?? "null"}.");
+            if (NotEqualTrimmed(entry.Link, submission.Link)) differences.Add($"Different Link: {entry.Link ?? "null"} vs {submission.Link ?? "null"}.");
+            if (NotEqualTrimmed(entry.Link2, submission.Link2)) differences.Add($"Different Link2: {entry.Link2 ?? "null"} vs {submission.Link2 ?? "null"}.");
+            if (NotEqualTrimmed(entry.Description, submission.Description)) differences.Add($"Different Description: {entry.Description ?? "null"} vs {submission.Description ?? "null"}.");
+            if (NotEqualTrimmed(entry.Location, submission.Location)) differences.Add($"Different Location: {entry.Location ?? "null"} vs {submission.Location ?? "null"}.");
+            if (NotEqualTrimmed(entry.Processor, submission.Processor)) differences.Add($"Different Processor: {entry.Processor ?? "null"} vs {submission.Processor ?? "null"}.");
+            if (NotEqualTrimmed(entry.Note, submission.Note)) differences.Add($"Different Note: {entry.Note ?? "null"} vs {submission.Note ?? "null"}.");
+            if (NotEqualTrimmed(entry.Contact, submission.Contact)) differences.Add($"Different Contact: {entry.Contact ?? "null"} vs {submission.Contact ?? "null"}.");
+            if (entry.SubCategoryId != submission.SubCategoryId) differences.Add($"Different SubCategoryId: {entry.SubCategoryId} vs {submission.SubCategoryId}.");
+            if (entry.DirectoryStatus != submission.DirectoryStatus) differences.Add($"Different DirectoryStatus: {entry.DirectoryStatus} vs {submission.DirectoryStatus}.");
+ 
+            return differences.Count > 0 ? string.Join(Environment.NewLine, differences) : "No differences found.";
+        }
+
 
     }
 }
