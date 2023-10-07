0b360bc5560ebfe7f8e5ff8fe640323ed818ee6c
Ryan Williams <ryanmichaelwilliams@gmail.com>
2023-10-06 21:48:07 -0600

    footer change

--- src/DirectoryManager/DirectoryManager.Data/Repositories/Interfaces/IDirectoryEntryRepository.cs
+++ src/DirectoryManager/DirectoryManager.Data/Repositories/Interfaces/IDirectoryEntryRepository.cs
@@ -12,15 +12,11 @@ namespace DirectoryManager.Data.Repositories.Interfaces
         Task UpdateAsync(DirectoryEntry entry);
         Task DeleteAsync(int id);
         public DateTime GetLastRevisionDate();
-
         public Task<IEnumerable<DirectoryEntry>> GetNewestRevisions(int count);
-
         public Task<IEnumerable<GroupedDirectoryEntry>> GetNewestAdditionsGrouped(int numberOfDays);
         public Task<IEnumerable<GroupedDirectoryEntry>> GetNewestAdditionsGrouped(int pageSize, int pageNumber);
         public Task<IEnumerable<DirectoryEntry>> GetNewestAdditions(int count);
-
         public Task<IEnumerable<DirectoryEntry>> GetActiveEntriesByCategoryAsync(int subCategoryId);
-
         public Task<IEnumerable<DirectoryEntry>> GetAllEntitiesAndPropertiesAsync();
         public Task<int> TotalActive();
     }
