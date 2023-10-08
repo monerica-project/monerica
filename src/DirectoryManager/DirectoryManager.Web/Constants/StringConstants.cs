9c8ebf90f4c2fb4964674f19d09d59c696c6578a
Ryan Williams <ryanmichaelwilliams@gmail.com>
2023-10-08 11:06:37 -0600

    refactor date format

--- src/DirectoryManager/DirectoryManager.Web/Constants/StringConstants.cs
+++ src/DirectoryManager/DirectoryManager.Web/Constants/StringConstants.cs
@@ -3,5 +3,7 @@
     public class StringConstants
     {
         public const string EntriesCache = "entries-cache-key";
+        public const string DateFormat = "yyyy-MM-dd";
+        public const string DateTimeFormat = "yyyy-MM-dd HH:mm:ss";
     }
 }
