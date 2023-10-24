9c03e16c84051a5786802049ee9c859830bc2e9c
Ryan Williams <ryanmichaelwilliams@gmail.com>
2023-10-24 14:37:27 -0600

    refactor pay messages to db

--- src/DirectoryManager/DirectoryManager.Data/Enums/SiteConfigSetting.cs
+++ src/DirectoryManager/DirectoryManager.Data/Enums/SiteConfigSetting.cs
@@ -6,10 +6,12 @@
         HeaderHtml = 1,
         FooterHtml = 2,
         HomePageDisplayHtml = 3,
+        PaymentCurrencyMessage = 4,
         MetaTags = 5,
         CssHeader = 6,
         DirectoryLegendHtml = 7,
         HomePageTitle = 8,
         DonationHtml = 9,
+        NowPaymentsMessage = 10,
     }
 }
