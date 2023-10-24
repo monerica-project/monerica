ed43c97e0b3cc4d85e93b733b009c1209da304bd
Ryan Williams <ryanmichaelwilliams@gmail.com>
2023-10-23 21:31:39 -0600

    10 hours cache

--- src/DirectoryManager/DirectoryManager.Web/Constants/IntegerConstants.cs
+++ src/DirectoryManager/DirectoryManager.Web/Constants/IntegerConstants.cs
@@ -3,7 +3,7 @@
     public class IntegerConstants
     {
         public const int DefaultPageSize = 25;
-        public const int CacheDurationSeconds = 60 * 60;
+        public const int CacheDurationSeconds = 36000; // 10 hours
         public const int MaxSponsoredListings = 5;
     }
 }
