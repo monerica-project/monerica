40ad0abbcfca240382ea00a2ef625637a9feef58
Ryan Williams <ryanmichaelwilliams@gmail.com>
2024-01-28 10:40:46 -0500

    admin submission note

---
+++ src/DirectoryManager/DirectoryManager.Data/Migrations/20240128153935_AdminNoteOnSubmission.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DirectoryManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminNoteOnSubmission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NoteToAdmin",
+                table: "Submissions",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NoteToAdmin",
+                table: "Submissions");
+        }
+    }
+}
