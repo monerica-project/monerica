90f8c7d555b43ee618b1aab3942ccaa5ed88a950
Ryan Williams <ryanmichaelwilliams@gmail.com>
2024-02-14 08:48:38 -0500

    email management

---
+++ src/DirectoryManager/DirectoryManager.Web/Controllers/EmailSubscriptionManagementController.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+using DirectoryManager.Data.Repositories.Interfaces;
+using DirectoryManager.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DirectoryManager.Web.Controllers
+{
+    [Authorize]
+    public class EmailSubscriptionManagementController : Controller
+    {
+        private readonly IEmailSubscriptionRepository emailSubscriptionRepository;
+
+        public EmailSubscriptionManagementController(
+            IEmailSubscriptionRepository emailSubscriptionRepository)
+        {
+            this.emailSubscriptionRepository = emailSubscriptionRepository;
+        }
+
+        [Route("account/emailsubscriptionmanagement")]
+        public IActionResult Index()
+        {
+            var allEmails = this.emailSubscriptionRepository.GetAll();
+            var model = new EmailSubscribeEditListModel();
+
+            foreach (var sub in allEmails)
+            {
+                model.Items.Add(new EmailSubscribeEditModel()
+                {
+                    Email = sub.Email,
+                    IsSubscribed = sub.IsSubscribed,
+                    EmailSubscriptionId = sub.EmailSubscriptionId
+                });
+            }
+
+            if (allEmails != null && allEmails.Count > 0)
+            {
+                var sb = new StringBuilder();
+                foreach (var sub in allEmails)
+                {
+                    if (!sub.IsSubscribed)
+                    {
+                        continue;
+                    }
+
+                    sb.AppendFormat("{0}, ", sub.Email);
+                }
+
+                model.Emails = sb.ToString();
+                model.Emails = model.Emails.Trim().TrimEnd(',');
+            }
+
+            var link = string.Format(
+                "{0}://{1}/emailsubscription/Unsubscribe",
+                this.HttpContext.Request.Scheme,
+                this.HttpContext.Request.Host);
+            model.UnsubscribeLink = link;
+
+            return this.View(model);
+        }
+
+        [Route("account/emailsubscriptionmanagement/edit")]
+        [HttpPost]
+        public IActionResult Edit(EmailSubscribeEditModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var dbModel = this.emailSubscriptionRepository.Get(model.EmailSubscriptionId);
+
+            dbModel.Email = model.Email;
+            dbModel.IsSubscribed = model.IsSubscribed;
+
+            this.emailSubscriptionRepository.Update(dbModel);
+
+            return this.RedirectToAction("index");
+        }
+
+        [Route("account/emailsubscriptionmanagement/edit")]
+        [HttpGet]
+        public IActionResult Edit(int emailSubscriptionId)
+        {
+            var dbModel = this.emailSubscriptionRepository.Get(emailSubscriptionId);
+
+            var model = new EmailSubscribeEditModel()
+            {
+                Email = dbModel.Email,
+                IsSubscribed = dbModel.IsSubscribed,
+                EmailSubscriptionId = dbModel.EmailSubscriptionId
+            };
+
+            return this.View(model);
+        }
+    }
+}
---
+++ src/DirectoryManager/DirectoryManager.Web/Models/EmailSubscribeEditListModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DirectoryManager.Web.Models
+{
+    public class EmailSubscribeEditListModel
+    {
+        public List<EmailSubscribeEditModel> Items { get; set; } = new List<EmailSubscribeEditModel>();
+
+        public string Emails { get; set; } = default!;
+
+        public string UnsubscribeLink { get; set; } = default!;
+    }
+}
