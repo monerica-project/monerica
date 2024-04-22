0a86efe5e5005b4c22607d5d9f468fdc5675c017
Ryan Williams <ryanmichaelwilliams@gmail.com>
2024-04-22 11:24:18 -0600

    sub category sponsor links

--- src/DirectoryManager/DirectoryManager.Web/Models/SponsoredListingHomeModel.cs
+++ src/DirectoryManager/DirectoryManager.Web/Models/SponsoredListingHomeModel.cs
@@ -2,9 +2,10 @@
 {
     public class SponsoredListingHomeModel
     {
-        public bool CanCreateSponsoredListing { get; set; }
+        public bool CanCreateMainListing { get; set; }
         public DateTime NextListingExpiration { get; set; }
         public int CurrentListingCount { get; set; }
+        public Dictionary<int, string> AvailableSubCatetgories { get; set; } = new Dictionary<int, string>();
         public string? Message { get; set; }
     }
 }
