9182dda394c357913b98797a93f1f812cf761475
Ryan Williams <ryanmichaelwilliams@gmail.com>
2024-10-20 20:11:33 -0400

    resolve enum desc

--- src/DirectoryManager/DirectoryManager.Data/Enums/PaymentStatus.cs
+++ src/DirectoryManager/DirectoryManager.Data/Enums/PaymentStatus.cs
@@ -1,11 +1,16 @@
-﻿namespace DirectoryManager.Data.Enums
+﻿using System.ComponentModel;
+
+namespace DirectoryManager.Data.Enums
 {
     public enum PaymentStatus
     {
         Unknown = 0,
+        [Description("Invoice Created")]
         InvoiceCreated = 1,
         Pending = 2,
+        [Description("Under Payment")]
         UnderPayment = 3,
+        [Description("Over Payment")]
         OverPayment = 4,
         Paid = 5,
         Expired = 6,
