8dfe9ecb5b664dfaddd142a776800f31d7d2106d
Ryan Williams <ryanmichaelwilliams@gmail.com>
2024-11-06 18:07:33 -0500

    save mailing

---
+++ src/DirectoryManager/DirectoryManager.Services/Models/SendGridConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace DirectoryManager.Services.Models
+{
+    public class SendGridConfig
+    {
+        required public string ApiKey { get; set; }
+        required public string SenderEmail { get; set; }
+        required public string SenderName { get; set; }
+    }
+}
