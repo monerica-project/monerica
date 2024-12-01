cd4bbb86a127f88e704c4880459eb8e6a5cb8d03
Ryan Williams <ryanmichaelwilliams@gmail.com>
2024-12-01 07:40:35 -0500

    test email

---
+++ src/DirectoryManager/DirectoryManager.Web/Models/TestEmailViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DirectoryManager.Web.Models
+{
+    public class TestEmailViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string RecipientEmail { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(255, ErrorMessage = "Subject must be less than 255 characters.")]
+        public string Subject { get; set; } = "Test Email";
+
+        [Required]
+        public string BodyText { get; set; } = "This is a test email sent from the application.";
+
+        [Required]
+        public string BodyHtml { get; set; } = "<p>This is a <strong>test email</strong> sent from the application.</p>";
+    }
+}
