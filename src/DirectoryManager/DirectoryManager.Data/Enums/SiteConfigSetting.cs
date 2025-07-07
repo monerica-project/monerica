9b284b9a387f91781a829f5997637459db70552a
Ryan Williams <ryanmichaelwilliams@gmail.com>
2025-07-07 15:28:45 -0400

    use config

--- src/DirectoryManager/DirectoryManager.Data/Enums/SiteConfigSetting.cs
+++ src/DirectoryManager/DirectoryManager.Data/Enums/SiteConfigSetting.cs
@@ -41,6 +41,7 @@
         SiteLogoUrl = 36,
         CategorySponsoredListingDetails = 37,
         SponsoredListingSteps = 38,
-        SearchHtml = 39
+        SearchHtml = 39,
+        TextIntro = 40
     }
 }
