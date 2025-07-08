e76f5916ca2e31c5b62c93dca7c22246623c1137
Ryan Williams <ryanmichaelwilliams@gmail.com>
2025-07-08 11:04:07 -0400

    change date names

--- src/DirectoryManager/DirectoryManager.Data/Repositories/Interfaces/ISearchLogRepository.cs
+++ src/DirectoryManager/DirectoryManager.Data/Repositories/Interfaces/ISearchLogRepository.cs
@@ -11,6 +11,7 @@ namespace DirectoryManager.Data.Repositories.Interfaces
         /// Gets a report of all distinct search terms between <paramref name="start"/> (inclusive)
         /// and <paramref name="end"/> (exclusive), with counts, first/last, and percentage of total.
         /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation of report items.</returns>
         Task<IReadOnlyList<SearchReportItem>> GetReportAsync(DateTime start, DateTime end);
     }
 }
