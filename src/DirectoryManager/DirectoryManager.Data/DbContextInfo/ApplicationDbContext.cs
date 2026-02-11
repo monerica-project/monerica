7cc56045062931ad6cff0e9d26423a7465f76165
Ryan Williams <ryanmichaelwilliams@gmail.com>
2026-02-11 13:21:26 -0500

    csscss/fix

--- src/DirectoryManager/DirectoryManager.Data/DbContextInfo/ApplicationDbContext.cs
+++ src/DirectoryManager/DirectoryManager.Data/DbContextInfo/ApplicationDbContext.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.Reflection;
+﻿using System.Reflection;
 using DirectoryManager.Data.Models;
 using DirectoryManager.Data.Models.Affiliates;
 using DirectoryManager.Data.Models.BaseModels;
 using DirectoryManager.Data.Models.Emails;
 using DirectoryManager.Data.Models.Reviews;
 using DirectoryManager.Data.Models.SponsoredListings;
+using Microsoft.EntityFrameworkCore;
 
 namespace DirectoryManager.Data.DbContextInfo
 {
@@ -541,7 +541,7 @@ namespace DirectoryManager.Data.DbContextInfo
                             && m.GetParameters().Length == 0);
 
             var generic = method.MakeGenericMethod(typeof(T));
-            return generic.Invoke(this, null)!;
+            return generic.Invoke(this, null) !;
         }
     }
 }
