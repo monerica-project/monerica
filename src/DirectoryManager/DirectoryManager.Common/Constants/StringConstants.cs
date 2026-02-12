1c1bb3cdb4d88e6bc831717ba26e98d473a3a159
Ryan Williams <ryanmichaelwilliams@gmail.com>
2026-02-12 15:42:47 -0500

    revert format

--- src/DirectoryManager/DirectoryManager.Common/Constants/StringConstants.cs
+++ src/DirectoryManager/DirectoryManager.Common/Constants/StringConstants.cs
@@ -6,7 +6,8 @@
         public const string DateTimeFormatSiteMapXml = "yyyy-MM-dd'T'HH:mm:ss+00:00";
         public const string DateTimeFormat = "yyyy-MM-ddTHH:mm:ssZ";
         public const string DateTimeFormat2 = "yyyy-MM-ddTHH:mm:ss";
-        public const string DateTimeFormat3 = "yyyy-MM-ddTHH:mmZ";
+        public const string DateTimeFormat3 = "yyyy-MM-ddTHH:mm";
+        public const string DateTimeFormat4 = "yyyy-MM-ddTHH:mmZ";
         public const string DefaultCulure = "en-US";
         public const string CurrentFormat = "C";
         public const string CurrentFormat2 = "C2";
